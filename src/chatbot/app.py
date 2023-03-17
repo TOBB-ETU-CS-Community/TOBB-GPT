4fc59c6498b25824f49e8a00bda6fd96a00c9b22
Hüseyin PEKKAN <75019129+hpekkan@users.noreply.github.com>
2023-03-17 13:48:00 +0300

    Update app.py

--- src/chatbot/app.py
+++ src/chatbot/app.py
@@ -243,7 +243,7 @@ def main():
     config.speech_synthesis_language = "en-US"
     config.speech_synthesis_voice_name='en-US-JennyNeural'
     audio_config = sdk.audio.AudioOutputConfig(use_default_speaker=True)
-    speech_synthesizer = sdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    speech_synthesizer = sdk.SpeechSynthesizer(speech_config=config, audio_config=audio_config)
     
     #synthesizer = sdk.SpeechSynthesizer(speech_config=config)
     input_text = st.text_input("Please write a text to convert it to a speech:")
